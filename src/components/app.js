adf24ae0ec6b5fb322e3e1c4258fc173f2dacbfc
Hubert MUGABO <mugberto@gmail.com>
2021-08-03 11:58:45 +0200

    Add footer

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,9 @@
 import header from "./header/header";
 import './css/app.css';
+import footer from "./footer/footer";
 
 const app = () => {
-    return header()
+    return header() + footer();
 }
 
 export default app;
