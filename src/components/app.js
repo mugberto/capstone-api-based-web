1d5d055c3ca6d4e7df9196db9bfcd84cc44ef351
unknown <videnovic99@hotmail.com>
2021-08-05 01:05:42 +0200

    display popup movie window

--- src/components/app.js
+++ src/components/app.js
@@ -2,10 +2,10 @@ import header from "./header/header";
 import './css/app.css';
 import footer from "./footer/footer";
 import { movieList } from "./movieList/movieList";
-
+import { moviePopupTemplate } from './moviePopup/moviePopup';
 
 const app = () => {
-    return header() + movieList() + footer();
+    return header() + movieList() + footer() + moviePopupTemplate();
 }
 
 export default app;
---
+++ src/components/moviePopup/moviePopup.js
@@ -0,0 +1,67 @@
+import './css/moviePopup.css';
+
+export const showMoviePopup =  async (commentBtn) => {
+    await fetch(`https://api.themoviedb.org/3/movie/${commentBtn.dataset.key}?api_key=8c5cae2e7a2a28ebed0be230de325abe&language=en-US`)
+    .then((resp) => resp.json())
+    .then((data) => {
+        let genre = data.genres.map((genre) => genre.name);
+        genre = genre.join(', ');
+        console.log(genre);
+        const popupWrap = document.querySelector('.popupWrap');
+        popupWrap.style.display = 'flex';
+        popupWrap.innerHTML = `
+                            <div class="popupContentWrap">
+                                <div class="exit-btn">
+                                    <i class="far fa-times-circle"></i>
+                                </div>
+                                <div class="popupTop">
+                                    <div class="movie-image">
+                                        <img src="http://image.tmdb.org/t/p/w500/${data.poster_path}" alt="movie-poster" />
+                                    </div>
+                                    <div class="movie-details">
+                                        <h2>${data.title}</h2>
+                                        <p>Genre: <span>${genre}</span></p>
+                                        <p>Relase date: <span>${data.release_date}</span></p>
+                                        <p>Vote average: <span>${data.vote_average}</span></p>
+                                        <p>Description: <span>${data.overview}</span></p>
+                                    </div>
+                                </div>
+                                <div class="comments">
+                                    <h2>Comments<span>(2)</span></h2>
+                                    <ul class="comments-wrap">
+                                        <li class="comment">
+                                            <div class="comment-details">
+                                                <p class="date">03/08/2021</p>
+                                                <p class="name">Alex</p>
+                                            </div>
+                                            <p class="comment-text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has</p>
+                                        </li>
+                                        <li class="comment">
+                                            <div class="comment-details">
+                                                <p class="date">03/08/2021</p>
+                                                <p class="name">Alex</p>
+                                            </div>
+                                            <p class="comment-text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has</p>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <form class="add-comment-form"> 
+                                    <h2>Add Comment</h2>
+                                    <input type="text" name="name" placeholder="Your name" require />
+                                    <textarea name="comment" placeholder="Your insights" require></textarea>
+                                    <button class="add-comment-btn" type="button">Comment</button>
+                                </form>
+                            </div>
+        `;
+        const closePopup = document.querySelector('.popupContentWrap .exit-btn i');
+        closePopup.addEventListener('click', () => popupWrap.style.display = "none");
+        console.log(data)
+    })
+}
+
+export const moviePopupTemplate = () => {
+    return `
+        <div class="popupWrap"></div>
+    `
+}
+
